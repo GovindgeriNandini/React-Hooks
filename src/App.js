6b838d84379b1629a70c624bda40a8a0bad4bc97
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-24 19:19:53 +0530

    use-state for arrays

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import HookCounter2 from './components/HookCounter2';
 
 import HookCounter3 from './components/HookCounter3';
 
+import HookCounter4 from './components/HookCounter4';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <HookCounter />  */}
         {/* <HookCounter2/> */}
-        <HookCounter3 />
-
+        {/* <HookCounter3 /> */}
+        <HookCounter4/>
       </header>
     </div>
   );
