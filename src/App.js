55d34fdfa8bd4ccb7a8f0bd8ef62c054b8b931b0
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-24 19:29:21 +0530

    use-state with arrays

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HookCounter2 from './components/HookCounter2';
 
 import HookCounter3 from './components/HookCounter3';
 
-import HookCounter4 from './components/HookCounter4';
+import HookCounter4 from './HookCounter4';
 
 function App() {
   return (
