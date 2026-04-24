3851c0023a1d75fe1edf6fcfadc941032f47f9bb
GovindgeriNandini <nandinig2646@gmail.com>
2026-04-24 18:32:44 +0530

    Added a new function

--- src/components/HookCounter2.jsx
+++ src/components/HookCounter2.jsx
@@ -5,12 +5,20 @@ import React  from 'react';
 const HookCounter2 = () => {
     const initialCount = 0;
     const [count, setCount] = useState(initialCount);
+
+    const handleIncrement5 = () => {
+        for(let i = 0; i < 5 ; i++){
+            setCount(count+1);
+        }
+    };
+
     return(
         <div>
             <span>Count : {count} </span>
             <button onClick={() => setCount(initialCount)}> Reset </button>
             <button onClick={() => setCount(count+1)}> Increment </button>
             <button onClick={() => setCount(count-1)}> Decrement </button>
+            <button onClick={handleIncrement5}>Increment by 5 </button>
 
         </div>
 
